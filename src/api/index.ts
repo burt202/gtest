2e03d94c94ff7eaab8c0aac2a7dc817ba84c2be6
Aaron Burtnyk <burt202@gmail.com>
2019-08-31 21:14:41 +0100

    Add npm run serve script

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,11 @@
 import {ApolloServer, gql} from "apollo-server-express"
 import * as express from "express"
 
-// Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
     hello: String
   }
 `
-
-// Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
     hello: () => "Hello world!",
@@ -20,7 +17,15 @@ const server = new ApolloServer({typeDefs, resolvers})
 const app = express()
 server.applyMiddleware({app})
 
+const currentDate = new Date().toLocaleString()
+
+app.get("/", (_, res: any) => {
+  res.send(
+    `see graphql at http://localhost:4000${server.graphqlPath} - refreshed on ${currentDate}`,
+  )
+})
+
 app.listen({port: 4000}, () =>
   // tslint:disable-next-line:no-console
-  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`),
+  console.log("Server ready at http://localhost:4000"),
 )
